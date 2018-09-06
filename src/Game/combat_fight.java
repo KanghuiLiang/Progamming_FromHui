4ccb15b93631c087f8d3854f8bdb09763817cb93
Mr.DiNz <poo.hu@hotmail.com>
2018-09-06 16:56:50 +0700

    Arraylist for test

--- src/Game/combat_fight.java
+++ src/Game/combat_fight.java
@@ -2,12 +2,12 @@ package Game;
 
 public class combat_fight {
     public static void main(String[] args) {
-        if (Monster.escapeStat > Randomizer(1, 6) {
-            Combat(Hero Player, Monster Monster);
-        }
-			else {
-            return "You escaped";
-        }
+//        if (Monster.escapeStat > Randomizer(1, 6) {
+//            Combat(Hero Player, Monster Monster);
+//        }
+//			else {
+//            return "You escaped";
+//        }
     }
 }
 
